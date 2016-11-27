397f99b1a28431577368fce7fb6507bd0b64af3e
Kevin Kwan <kk3439@nyu.edu>
2016-11-27 15:48:25 -0500

    Added getSet() and write()

--- cachesimulator.cpp
+++ cachesimulator.cpp
@@ -62,7 +62,7 @@ class cache {
 		cachesize = (int)pow(2, temp);
 		offsetBits = log(blocksize)/log(2);
 		indexnum = (cachesize/blocksize)/ways;
-		cout << "indexnum: " << indexnum << endl;
+		//cout << "indexnum: " << indexnum << endl;
 		setBits = (int)(log(indexnum)/log(2));
 		tagBits = 32 - (offsetBits + setBits);
 
@@ -73,13 +73,37 @@ class cache {
 		delete [] cb;
 	}
 
+	long getSet(bitset<32> iaddr){
+		//cout << "getSet input: " << iaddr << endl;
+		bitset<32> temp;
+		int start = offsetBits;
+		int end = offsetBits + setBits;
+		//cout << "start: " << start << ", end: " << end << endl;
+
+		//cout << "getSet = ";
+
+		for (int i = start, j = 0; i < end; i++, j++){
+			temp[j] = iaddr[i];
+			//cout << iaddr[i];
+		}
+		//cout << endl;
+/*
+		for (int i = start; i < end; i++)
+			cout << iaddr[i];
+		cout << endl;
+*/
+
+		//cout << "getSet = " << temp << endl;
+		return temp.to_ulong();
+	}
+
 	bitset<32> read (bitset<32> raddr, int waynum) {
 		bitset<32> index;
 		int start = offsetBits + 1;
 		int end = offsetBits + setBits;
 		long set;
 
-		for (int i = start, j = 0; i <= offsetBits; i++, j++)
+		for (int i = start, j = 0; i < (offsetBits+1); i++, j++)
 			index[j] = raddr[i];
 
 		set = index.to_ulong();
@@ -96,17 +120,38 @@ class cache {
 		else
 			return 0;
 	}
+
+	bool write (bitset<32> waddr) {
+		//cout << "input: " << waddr << endl;
+		long setnum = getSet(waddr);
+		//cout << "write: " << setnum << endl;
+
+		for (int i = 0; i < ways; i++){
+			bool written = compareTag(waddr, cb[setnum*ways+i].addr, tagBits);
+			if (written && cb[setnum*ways+i].valid == true) {
+				cb[setnum*ways+i].dirty = true;
+				//cout << "written: " << written << endl;
+				//cout << "dirty: " << cb[setnum*ways+i].dirty << endl;
+				return true;
+			}
+			else
+				return false;
+		}
+		//return true;
+	}
 };
 
 bool compareTag(bitset<32> addr1, bitset<32> addr2, int tagBits){
 	bitset<32> tag1, tag2;
-	int start = (32-tagBits) + 1;
+	int start = (32-tagBits);
+	//cout << "compareTag start: " << start << endl;
 
 	for (int i = start, j = 0; i < 32; i++, j++){
 		tag1[j] = addr1[i];
 		tag2[j] = addr2[i];
 	}
 
+	//cout << "compareTag tag1: " << tag1 << ", tag2: " << tag2 << endl;
 	if (tag1 == tag2)
 		return true;
 	else
@@ -144,12 +189,17 @@ int main(int argc, char* argv[]){
    // probably you may define a Cache class for L1 and L2, or any data structure you like
 
 	cache l1cache(cacheconfig.L1blocksize, cacheconfig.L1setsize, cacheconfig.L1size);
+	cout << "L1: block size = " << l1cache.blocksize << ", sets = " << l1cache.indexnum << ", ways = " << l1cache.ways << ", size = " << l1cache.cachesize << endl;
 	cout << "offset bits: " << l1cache.offsetBits << ", set bits: " << l1cache.setBits << ", tag bits: ";
 	cout << l1cache.tagBits << endl;
 
-	l1cache.cb[1*l1cache.indexnum + 0].addr = 0xbf984000;
-	cout << "[1][0] = " << l1cache.cb[1*l1cache.indexnum+0].addr << endl;
+	//l1cache.cb[1*l1cache.indexnum + 0].addr = 0xbf984000;
+	//cout << "[1][0] = " << l1cache.cb[1*l1cache.indexnum+0].addr << endl;
+	l1cache.cb[183*l1cache.ways + 0].addr = 0xbf9845b8;
+	l1cache.cb[183*l1cache.ways + 0].valid = true;
+	cout << "[183][0] = " << l1cache.cb[183*l1cache.ways+0].addr << endl;
 	cache l2cache(cacheconfig.L2blocksize, cacheconfig.L2setsize, cacheconfig.L2size);
+	cout << "L2: block size = " << l2cache.blocksize << ", sets = " << l2cache.indexnum << ", ways = " << l2cache.ways << ", size = " << l2cache.cachesize << endl;
    	cout << "offset bits: " << l2cache.offsetBits << ", set bits: " << l2cache.setBits << ", tag bits: ";
 	cout << l2cache.tagBits << endl;
 
@@ -212,8 +262,20 @@ int main(int argc, char* argv[]){
                   //and then L2 (if required), 
                   //update the L1 and L2 access state variable;
                   
-				  equal = compareTag(l1cache.cb[1*l1cache.indexnum+0].addr, accessaddr, l1cache.tagBits); 
-				  cout << accessnum << ": " << equal << endl;
+				  //equal = compareTag(l1cache.cb[183*l1cache.ways+0].addr, accessaddr, l1cache.tagBits); 
+				  //cout << accessnum << ": " << equal << endl;
+					cout << accessnum << ": ";
+					if (l1cache.write(accessaddr))
+						cout << "L1: " << WH << " L2: " << NA << endl;
+					else {
+						cout << "L1: " << WM;
+						if (l2cache.write(accessaddr))
+							cout << " L2: " << WH << endl;
+						else
+							cout << " L2: " << WM << endl;
+					}
+					if (accessnum > 15)
+						break;
                   
                   
                   
